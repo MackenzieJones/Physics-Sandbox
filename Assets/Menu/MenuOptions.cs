3f9f8038b08d2ec709003e049c421800ce016c90
Mackenzie Jones <mackenzie.jones@unb.ca>
2019-10-04 00:51:52 -0300

    Initial Dump

---
+++ Assets/Menu/MenuOptions.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuOptions: MonoBehaviour {
+
+	void Start() {
+
+	}
+
+	void Update() {
+
+	}
+
+	public void loadMain() {
+		SceneManager.LoadSceneAsync("Main");
+	}
+
+}
