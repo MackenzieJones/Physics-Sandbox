fd7413e5b6ab7f9a254f1f73c26a657a7697dcc4
Mackenzie Jones <mackenzie.jones@unb.ca>
2019-10-09 16:45:01 -0300

    Completed alpha create new option

--- Assets/Main/Create New/CreateNew.cs
+++ Assets/Main/Create New/CreateNew.cs
@@ -1,15 +1,19 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class CreateNew: MonoBehaviour {
 
 	public float mass;
 	public float bounciness;
-	public float r, g, b;
+	public float r, g, b, prevRGB;
 	public float size, prevSize;
 	private GameObject shape;
+	private int shapeIndex;
 
-	public GameObject[] shapes; //Initalized in unity
+	public GameObject[] uiShapes; //Initalized in unity
+	public GameObject[] realShapes; //Initalized in unity
 	public GameObject[] sliders; //Initalized in unity
 
 	public GameObject previewObject; //Initialized in unity with dummy object for location
@@ -19,14 +23,22 @@ public class CreateNew: MonoBehaviour {
 	private float minSize = 20;
 	private float maxSize = 300;
 
+	private List<GameObject> loadedObjects;
+
 	void Start() {
-		previewObject = Instantiate(shapes[0], previewObject.transform.position, Quaternion.identity, canvas.transform);
-		
+		loadedObjects = new List<GameObject>();
+		previewObject = Instantiate(uiShapes[0], previewObject.transform.position, Quaternion.identity, canvas.transform);
+
 		prevSize = 0;
 	}
 
 	void Update() {
+		updateColour();
 		updateSize();
+		//TODO: add animation for bounce selection
+		//updateBounce(); 
+		//TODO: add graphic for mass change
+		//updateMass();
 	}
 
 	private void updateSize() {
@@ -35,11 +47,53 @@ public class CreateNew: MonoBehaviour {
 			previewObject.transform.localScale = new Vector3((size + minSize * 1 / maxSize) * (maxSize - minSize), (size + minSize * 1 / maxSize) * (maxSize - minSize), 1);
 		}
 		prevSize = size;
-		
+
 	}
 
-	public void pickCircle() { shape = shapes[0]; }
-	public void pickHex() { shape = shapes[1]; }
-	public void pickSquare() { shape = shapes[2]; }
-	public void pickTriangle() { shape = shapes[3]; }
+	private void updateColour() {
+		r = sliders[2].GetComponent<Slider>().value;
+		g = sliders[3].GetComponent<Slider>().value;
+		b = sliders[4].GetComponent<Slider>().value;
+
+		if (r + b + g != prevRGB) {
+			previewObject.GetComponent<SpriteRenderer>().color = new Color(r, g, b);
+		}
+
+		prevRGB = r + b + g;
+	}
+
+
+	private void updateBounce() {
+		throw new NotImplementedException();
+	}
+
+	private void updateMass() {
+		throw new NotImplementedException();
+	}
+
+	public void pickShape(int shapeChoice) {
+		shapeIndex = shapeChoice;
+		shape = uiShapes[shapeIndex];
+		previewObject.GetComponent<SpriteRenderer>().sprite = shape.GetComponent<SpriteRenderer>().sprite;
+		previewObject.GetComponent<SpriteRenderer>().color = new Color(r, g, b);
+		previewObject.transform.localScale = new Vector3((size + minSize * 1 / maxSize) * (maxSize - minSize), (size + minSize * 1 / maxSize) * (maxSize - minSize), 1);
+		previewObject.transform.SetParent(gameObject.transform);
+	}
+
+	public void spawnNewObject() {
+		GameObject newObject = Instantiate(realShapes[shapeIndex], new Vector3(0, 0, 0), Quaternion.identity);
+
+		newObject.transform.localScale = new Vector3(size * 2.7f, size * 2.7f, 1);
+
+		newObject.GetComponent<SpriteRenderer>().color = new Color(r, g, b);
+
+		newObject.GetComponent<Rigidbody2D>().mass = sliders[0].GetComponent<Slider>().value * 20;
+
+		PhysicsMaterial2D mat2D = new PhysicsMaterial2D();
+		mat2D.bounciness = sliders[1].GetComponent<Slider>().value * 2f;
+		mat2D.friction = 2;
+		newObject.GetComponent<Collider2D>().sharedMaterial = mat2D;
+
+	}
+
 }
---
+++ Assets/Main/Create New/CreateNewButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CreateNewButton: MonoBehaviour {
+
+	public GameObject menu; //Initialized in unity
+
+	void Start() {
+
+	}
+
+	void Update() {
+
+	}
+
+	public void loadCreateNewMenu() {
+		menu.SetActive(true);
+	}
+
+	public void unloadCreateNewMenu() {
+		menu.SetActive(false);
+	}
+}
