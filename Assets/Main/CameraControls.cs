2ab2d15ec8c8cb7350ef2c1a551b971edf54d595
MackenzieJones <mackenzie.jones@unb.ca>
2019-11-05 22:55:01 -0400

    1/Session Edit Pt. 2 (#14)
    * Added basic session functionality
    
    * ADded background selection
    
    * Backgrounds complete
    
    * Created tools panel and added session-edit to it
    
    * Final fixes

--- Assets/Main/CameraControls.cs
+++ Assets/Main/CameraControls.cs
@@ -5,48 +5,85 @@
 
 using UnityEngine;
 
-public class CameraControls: MonoBehaviour {
+public class CameraControls : MonoBehaviour {
 	public float perspectiveZoomSpeed = 0.5f;
 	public float orthoZoomSpeed = 0.5f;
 	public Camera cam;
 
-	private Touch touchZero;
-	private Touch touchOne;
+	private Touch t1;
+	private Touch t2;
+
+	private float minMovement;
+	private float currMovement;
 
 	private Vector2 touchZeroPrevPos;
 	private Vector2 touchOnePrevPos;
+
+	private bool firstTouch;
+	private Vector3 worldPin;
+	private Vector3 worldTouch;
+
 
 	private float prevTouchDeltaMag;
 	private float touchDeltaMag;
 
 	private float deltaMagnitudeDiff;
 
+	private MainDirector director;
+
 	public void Start() {
 		cam = gameObject.GetComponent<Camera>();
+		director = GameObject.FindGameObjectWithTag("Director").GetComponent<MainDirector>();
+		minMovement = 1;
+		firstTouch = true;
 	}
 
 	void Update() {
 
-		if (Input.touchCount == 2) {
+		if (!director.isPaused()) {
+			if (Input.touchCount == 1) {
+				t1 = Input.GetTouch(0);
 
-			touchZero = Input.GetTouch(0);
-			touchOne = Input.GetTouch(1);
+				if (firstTouch) {
+					worldPin = Camera.main.ScreenToWorldPoint(t1.position);
+					firstTouch = false;
+				}
+				worldTouch = Camera.main.ScreenToWorldPoint(t1.position);
 
-			touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
-			touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+				Camera.main.transform.position += worldPin - worldTouch;
 
-			prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
-			touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
-
-			deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
-
-			if (cam.orthographic) {
-				cam.orthographicSize += deltaMagnitudeDiff * orthoZoomSpeed;
-				cam.orthographicSize = Mathf.Max(cam.orthographicSize, 0.1f);
 			} else {
-				cam.fieldOfView += deltaMagnitudeDiff * perspectiveZoomSpeed;
-				cam.fieldOfView = Mathf.Clamp(cam.fieldOfView, 0.1f, 179.9f);
+				firstTouch = true;
+				currMovement = 0;
 			}
+
+			if (Input.touchCount >= 2) {
+
+				t1 = Input.GetTouch(0);
+				t2 = Input.GetTouch(1);
+
+				touchZeroPrevPos = t1.position - t1.deltaPosition;
+				touchOnePrevPos = t2.position - t2.deltaPosition;
+
+				prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+				touchDeltaMag = (t1.position - t2.position).magnitude;
+
+				deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+
+				if (cam.orthographic) {
+					cam.orthographicSize += deltaMagnitudeDiff * orthoZoomSpeed;
+					cam.orthographicSize = Mathf.Max(cam.orthographicSize, 0.1f);
+				} else {
+					cam.fieldOfView += deltaMagnitudeDiff * perspectiveZoomSpeed;
+					cam.fieldOfView = Mathf.Clamp(cam.fieldOfView, 0.1f, 179.9f);
+				}
+
+			}
+		} else {
+			firstTouch = true;
+			currMovement = 0;
 		}
+
 	}
+
 }
---
+++ Assets/Main/Session/SessionEdit.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SessionEdit : MonoBehaviour {
+
+	public Dropdown bgChoice;
+	public GameObject background;
+	public Sprite[] bgs;
+
+	private float width;
+	private float height;
+
+	private float initialWidth;
+	private float initialHeight;
+
+	private float maxBoundrySize;
+
+	private Vector3 boundryInitialSize;
+
+	public Toggle boundryToggle;
+	public Toggle roofToggle;
+
+	public Slider[] sizes;
+	public Text[] displayValues;
+
+	public GameObject boundry;
+	public GameObject roof;
+	private MenuManager manager;
+
+	void Start() {
+		manager = GameObject.FindGameObjectWithTag("Director").GetComponent<MenuManager>();
+		initialWidth = sizes[0].value;
+		initialHeight = sizes[1].value;
+		boundryInitialSize = boundry.transform.localScale;
+		maxBoundrySize = 3;
+	}
+
+	void FixedUpdate() {
+		boundry.transform.localScale = new Vector3(boundryInitialSize.x * (1 + (sizes[0].value - initialWidth) * maxBoundrySize), boundryInitialSize.y * (1 + (sizes[1].value - initialHeight) * maxBoundrySize), 1);
+	}
+
+	public void backgroundChange() {
+		background.GetComponent<SpriteRenderer>().sprite = bgs[bgChoice.value];
+	}
+
+	public void boundryChange() {
+		if (boundryToggle.isOn) {
+			//TODO: grey out everything under "boundry"
+			boundry.SetActive(true);
+		} else {
+			//TODO: restore colour to everything under "boundry"
+			boundry.SetActive(false);
+		}
+	}
+
+	public void roofChange() {
+		if (roofToggle.isOn) {
+			roof.SetActive(true);
+		} else {
+			roof.SetActive(false);
+		}
+	}
+
+	public void undoChanges() {
+
+	}
+
+	public void closePane() {
+		manager.unloadEditSessionMenu();
+	}
+}
